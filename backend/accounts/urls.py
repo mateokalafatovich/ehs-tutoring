7705dd62b46e33c3f6f8e2ec636b370e2d9e80be
Mateo Kalafatovich <148184139+mateokalafatovich@users.noreply.github.com>
2026-06-23 14:30:07 -0700

    Set up URLs for users

---
+++ backend/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from accounts .views import *
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('verify/', VerifyEmailView.as_view(), name='verify-email'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('change-password/', UserChangePasswordView.as_view(), name='change-password'),
+    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
+    path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+]
